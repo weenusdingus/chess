46f4d4224aeb22f921c97665805be9fe619d3ae5
Remington Bassett <bassettremington@gmail.com>
2024-10-22 18:56:16 -0600

    passing some - implemented register and clear in server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,30 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.*;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
+import service.serviceExceptions.AlreadyTakenException;
+import service.serviceExceptions.BadRequestException;
+import service.serviceExceptions.UnauthorizedException;
 import spark.*;
 
 public class Server {
+    private final UserService userService;
+    private final GameService gameService;
+    private final ClearService clearService;
+    private final DataAccess dataaccess;
+
+    public Server() {
+        dataaccess = new MemoryDataAccess();
+        userService = new UserService(dataaccess);
+        gameService = new GameService(dataaccess);
+        clearService = new ClearService(dataaccess);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,9 +32,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.delete("/db", this::clearApplication);
+        Spark.post("/user", this::register);
+//        Spark.post("/session", this::login);
+//        Spark.delete("/session", this::logout);
+//        Spark.get("/game", this::listGames);
+//        Spark.post("/game", this::createGame);
+//        Spark.put("/game", this::joinGame);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,5 +47,33 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+    record ErrorMessage (String message){}
+    public Object clearApplication(Request req, Response res) throws DataAccessException {
+        try {
+            clearService.clear();
+            res.status(200);
+            return "";
+        } catch (DataAccessException e) {
+            res.status(500);
+            return new Gson().toJson(new ErrorMessage("Error: " + e.getMessage()));
+        }
+    }
+    public Object register(Request req, Response res) throws AlreadyTakenException, DataAccessException, BadRequestException {
+        try {
+            UserData user=new Gson().fromJson(req.body(), UserData.class);
+            AuthData auth = userService.register(user);
+            res.status(200);
+            return new Gson().toJson(auth);
+        } catch(BadRequestException e) {
+            res.status(400);
+            return new Gson().toJson(new ErrorMessage("Error: bad request"));
+        } catch (AlreadyTakenException e){
+            res.status(403);
+            return new Gson().toJson(new ErrorMessage("Error: already taken"));
+        } catch (Exception e) {
+            res.status(500);
+            return new Gson().toJson(new ErrorMessage("Error: " + e.getMessage()));
+        }
     }
 }
